32f4b115b2cab5761632b16540c9f3ec06a905d9
Miguel Arango <djmiguelarango@gmail.com>
2015-09-08 22:33:08 -0400

    Modelos

--- project-02/resources/views/home.php
+++ project-02/resources/views/home.php
@@ -6,5 +6,17 @@
 </head>
 <body>
     <h1><?= $message ;?></h1>
+
+    <ul>
+        <?php /** @type App\Post $post */
+        foreach($posts as $post): ?>
+            <li>
+                <h2><?= $post->getTitle() ;?> by <small><?= $post->getAuthor() ;?></small></h2>
+                <p>
+                    <?= $post->getBody() ;?>
+                </p>
+            </li>
+        <?php endforeach ?>
+    </ul>
 </body>
 </html>
--- project-02/src/Http/Controllers/HomeController.php
+++ project-02/src/Http/Controllers/HomeController.php
@@ -3,16 +3,29 @@
 namespace App\Http\Controllers;
 
 
+use App\Author;
 use App\Http\Views\View;
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController
 {
     public function index(Request $request)
     {
+        $author = new Author('djmiguelarango@gmail.com', 'secret', 'AUTHOR');
+        $author->setName('Miguel', 'MGM');
+
+        $posts = [
+            new Post($author, 'Post #1', 'This is first post'),
+            new Post($author, 'Post #2', 'This is second post'),
+            new Post($author, 'Post #3', 'This is third post'),
+            new Post($author, 'Post #4', 'This is fourth post'),
+        ];
+
         // var_dump($request);
         $params = [
             'message' => 'Hello from a View!',
+            'posts'   => $posts,
         ];
 
         $view = new View('home', $params);
